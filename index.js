bb45cab185310f7f09cafe61f10f12c1452a8581
Tinus Strydom <tinuss@loans-inc.co.za>
2019-06-08 12:05:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // write your code below!
+function happyHolidays(){
+  return "Happy Holidays";
+}
+functions happyHolidaysTo(name){
+  return `Happy Holidays, ${name}!`;
+}
+function happyHolidayTo(holiday, name){
+  return `Happy ${holiday}, ${name}!`;
+}
+function holidayCountdown(days, holiday){
+  return `It's ${days} days until ${holiday}`;
+}
+happyHolidays();
+happyHolidaysTo("Tinus");
+happyHolidayTo("Chrismas", "Tinus");
+holidayCountdown(240, "Christmas");
